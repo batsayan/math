5f43d5577b664f9a9d2a719c8176d90ec2090f81
batsayan <batsayan.das@tcs.com>
2019-11-25 15:17:23 +0530

    input from console as a string

--- math.c
+++ math.c
@@ -1,14 +1,16 @@
 /* C Program to Calculate Square of a Number */
  
 #include<stdio.h>
+#include <stdlib.h>
  
-int main()
+int main(int argc, char *argv[])
 {
   int number, Square;
- 
-  printf(" \n Please Enter any integer Value : ");
-  scanf("%d", &number);
-  
+  char *a = argv[1];
+
+//  printf(" \n Please Enter any integer Value : ");
+  //scanf("%d", &number);
+  number = atoi(a);
   Square = number * number;
   
   printf("\n Square of a given number %d is  =  %d\n", number, Square);
